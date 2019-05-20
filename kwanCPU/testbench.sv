d92fe7a8db978da00ba0e206a3403148c135d41a
Chris Jeppesen (Omoikane) <omoikane.kwansystems.org>
2019-05-20 15:39:19 +0000

    Added clock input to ALUFlags so that we can test flag in/out

--- kwanCPU/testbench.sv
+++ kwanCPU/testbench.sv
@@ -4,11 +4,12 @@ module test_full_add;
   reg [N-1:0]a;
   reg [N-1:0]b;
   reg su;
+  reg clk;
   wire [N-1:0]bus;
   wire cf,zf;
   
   // Instantiate design under test
-  ALU #(8) dut (.a(a), .b(b), .su(su), .bus(bus), .cf(cf),.zf(zf),.eo_('0));
+  ALUFlags #(8) dut (.a(a), .b(b), .su(su), .bus(bus), .cf(cf),.zf(zf),.eo_('0),.fi_('0),.clk(clk));
           
   initial begin
     // Dump waves
@@ -19,24 +20,36 @@ module test_full_add;
     a = 0;
     b = 0;
     su = 0;
+    clk=1;
+    display;
+    clk=~clk;
     display;
     
     $display("a=1");
     a = 34;
+    clk=~clk;
+    display;
+    clk=~clk;
     display;
 
     $display("b=1");
     b = 12;
+    clk=~clk;
+    display;
+    clk=~clk;
     display;
 
     $display("su=1");
     su = 1;
+    clk=~clk;
+    display;
+    clk=~clk;
     display;
   end
   
   task display;
     #1 $display("a:%0d, b:%0d, su:%0h, s:%0d, k:%0h",
-      a, b, su, bus, k);
+      a, b, su, bus, cf);
   endtask
 
 endmodule
